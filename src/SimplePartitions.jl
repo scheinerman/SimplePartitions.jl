ca5e99a439b156b648c6cebbc7dc20302a78b3b7
Ed Scheinerman <ers@jhu.edu>
2016-06-13 10:44:40 -0400

    Scant start to source

--- src/SimplePartitions.jl
+++ src/SimplePartitions.jl
@@ -1,1 +1,18 @@
 # nothing here
+module SimplePartitions
+using DataStructures
+
+type Partition{T}
+  elements::Set{T}
+  parts::DisjointSets{T}
+
+  function Partition{T}(A::Set{T})
+    elements = deepcopy(A)
+    parts = DisjointSets{T}(A)
+    P = new(elements,parts)
+  end
+end
+
+
+
+end  # end of module
