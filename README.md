b4a8ed1bd755d1b631ec1b9040db96e6721667f2
Ed Scheinerman <ers@jhu.edu>
2016-06-13 16:51:58 -0400

    Constructor successfully constructed

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Module for set partitions. This is a work in progress. We define a
 `Partition` to be a wrapper around the `DisjointUnion` type defined
 in the `DataStructures` module, but with a bit more functionality.
+
+
+## Constructor
+
+A new `Partition` is created by specifying the ground set. That is, if `A`
+is a `Set{T}` (for some type `T`) or an `IntSet`, then `Partition(A)` creates
+a new `Partition` whose ground set is `A` and the parts are singletons.
--- src/SimplePartitions.jl
+++ src/SimplePartitions.jl
@@ -1,14 +1,23 @@
-# nothing here
 module SimplePartitions
 using DataStructures
 
-export Partition
+export Partition, set_element_type
+
+"""
+`set_element_type(A)` gives the element type of a set `A`.
+"""
+function set_element_type{T}(A::Set{T})
+  return T
+end
+
+set_element_type(B::IntSet) = Int
+
 
 type Partition{T}
   elements::Set{T}
   parts::DisjointSets{T}
 
-  function Partition(A::Set{T})
+  function Partition{T}(A::Set{T})
     elts = deepcopy(A)
     pts  = DisjointSets{T}(A)
     new(elts,pts)
@@ -21,6 +30,20 @@ type Partition{T}
   end
 end
 
+"""
+A `Partition` is a set of nonempty, pairwise disjoint sets.
+A new `Partition` is created by specifying the ground set `A`
+and calling `Partition(A)`. The set `A` may be either a `Set{T}`
+for some type `T` or an `IntSet`.
 
+The datatype `Partition` is, essentially, a wrapper around the
+`DataStructures.DisjointSets` type.
+"""
+function Partition(A::Set)
+  T = set_element_type(A)
+  return Partition{T}(A)
+end
+
+Partition(B::IntSet) = Partition{Int}(B)
 
 end  # end of module
