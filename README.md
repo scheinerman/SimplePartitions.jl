328dcf5bcdf2f9d3ac1f0c6fe3a3d63bb2e6eb8e
Ed Scheinerman <ers@jhu.edu>
2016-06-13 22:02:38 -0400

    Good content

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ in the `DataStructures` module, but with a bit more functionality.
 A new `Partition` is created by specifying the ground set. That is, if `A`
 is a `Set{T}` (for some type `T`) or an `IntSet`, then `Partition(A)` creates
 a new `Partition` whose ground set is `A` and the parts are singletons.
+```julia
+julia> using ShowSet
+WARNING: Method definition show(Base.IO, Base.Set) ...
+WARNING: Method definition show(Base.IO, Base.IntSet) ...
+
+julia> using SimplePartitions
+
+julia> A = Set(1:10)
+{1,2,3,4,5,6,7,8,9,10}
+
+julia> P = Partition(A)
+Partition of {1,2,3,4,5,6,7,8,9,10} into 10 parts
+```
+
+## Functions
+
++ `num_elements(P)`: returns the number of elements in the ground
+set of `P`.
++ `num_parts(P)`: returns the number of parts in `P`.
--- src/SimplePartitions.jl
+++ src/SimplePartitions.jl
@@ -1,7 +1,10 @@
 module SimplePartitions
 using DataStructures
 
-export Partition, set_element_type
+import Base.show
+
+export Partition, set_element_type, num_elements, num_parts, parts
+export elements, has, merge_parts!
 
 """
 `set_element_type(A)` gives the element type of a set `A`.
@@ -9,7 +12,6 @@ export Partition, set_element_type
 function set_element_type{T}(A::Set{T})
   return T
 end
-
 set_element_type(B::IntSet) = Int
 
 
@@ -30,6 +32,7 @@ type Partition{T}
   end
 end
 
+
 """
 A `Partition` is a set of nonempty, pairwise disjoint sets.
 A new `Partition` is created by specifying the ground set `A`
@@ -43,7 +46,94 @@ function Partition(A::Set)
   T = set_element_type(A)
   return Partition{T}(A)
 end
-
 Partition(B::IntSet) = Partition{Int}(B)
+
+function show(io::IO, P::Partition)
+  print(io, "Partition of $(P.elements) into $(num_parts(P)) parts")
+end
+
+"""
+`has(P,a)` checks if `a` is in the ground set of `P`.
+"""
+function has{T}(P::Partition{T}, a::T)
+  return in(a,P.elements)
+end
+
+"""
+`merge_parts!(P,a,b)` updates `P` by merging the parts
+that contain elements `a` and `b`.
+"""
+function merge_parts!{T}(P::Partition{T},a::T,b::T)
+  if !has(P,a) || !has(P,b)
+    error("One or both of these elements are not in the partition.")
+  end
+  union!(P.parts,a,b)
+  nothing
+end
+
+
+"""
+`num_parts(P)` gives the number of parts in the partition `P`.
+"""
+num_parts(P::Partition) = num_groups(P.parts)
+
+"""
+`num_elements(P)` gives the number of elements in the ground
+set of the partition `P`. This equals the sum of the sizes of the
+parts.
+"""
+num_elements(P::Partition) = length(P.elements)
+
+"""
+`elements(P)` returns (a copy of) the ground set of the
+partition `P`.
+"""
+elements(P::Partition) = deepcopy(P.elements)
+
+"""
+`parts(P)` returns a set containing the parts of the partition `P`.
+That is, we return a set of sets.
+"""
+function parts{T}(P::Partition{T})
+  n = num_parts(P)
+  GS = P.elements
+
+  # Find the indices of the roots of the parts
+  root_set = Set{Int}()
+  for item in GS
+    r = find_root(P.parts,item)
+    push!(root_set,r)
+  end
+  roots = collect(root_set)  # make it an array
+
+  # Create a mapping from root numbers to [1:n]
+  rootmap = Dict{Int,Int}()
+  for k=1:n
+    rootmap[roots[k]] = k
+  end
+
+  # Create an array of sets to hold the parts
+  plist = Array(Set{T},n)
+  for k=1:n
+    plist[k] = Set{T}()  # make sure they're empty sets
+  end
+
+  # For each element in the ground set, look up its root
+  # in order to place it in its appropriate set.
+  for item in GS
+    idx = rootmap[find_root(P.parts,item)]
+    push!(plist[idx], item)
+  end
+
+  # now take those parts and pack them into a set
+  S = Set{Set{T}}()
+  for item in plist
+    push!(S,item)
+  end
+
+  return S
+end
+
+
 
 end  # end of module
